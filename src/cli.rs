f0d25fc9ab4994fbf60782f0c7ab887bbef60a86
Terence Lee <hone02@gmail.com>
2021-02-09 12:11:27 -0600

    add help text

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,17 @@
 use clap::Clap;
 use std::str::FromStr;
 
+/// Tool for converting TOML files to a set key/value files/folders
 #[derive(Clap)]
 #[clap(version = "0.1", author = "Terence Lee <hone02@gmail.com>")]
 pub struct Cli {
+    /// 'explode' take a TOML file and convert to a filesystem layout.
+    /// 'implode' will take a filesystem layout and construct a TOML file.
     #[clap(possible_values=&["explode", "implode"])]
     pub cmd: Command,
+    /// Path to the source input
     pub source: String,
+    /// Path to where the output is written
     pub destination: String,
 }
 
